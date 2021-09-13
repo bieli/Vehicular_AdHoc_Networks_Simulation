15c5b7335b4cc6e598dd8c7ba844e44a44705eef
Marcin Bielak <marcin.bieli@gmail.com>
2021-09-14 01:30:26 +0200

    upgrade PEPs rules in code

--- communication_terminal.py
+++ communication_terminal.py
@@ -23,30 +23,29 @@ This program is distributed in the hope that it will be useful,
 This module defines the class of the vehicular communication terminal.
 """
 
+
 class Communication_ternimal(object):
-    def __init__(self,
-                 ID, # terminal ID
-                 received_flag = False): # flag indicating whether receiving a message
+    def __init__(
+        self, ID, received_flag=False  # terminal ID
+    ):  # flag indicating whether receiving a message
         self.ID = ID
         self.received_flag = received_flag
         self.buffer_max_length = 100
         self.message_buffer = []
-        
-    def receive(self,message):        
+
+    def receive(self, message):
         if len(self.message_buffer) < self.buffer_max_length:
             self.message_buffer.append(message)
-            
+
         if self.message_buffer:
             self.received_flag = True
         else:
             self.received_flag = False
-    
+
     def update_buffer(self):
         # more complicated buffering protocol should be done in future
         pass
-    
-    def send(self,neighboring_node):
+
+    def send(self, neighboring_node):
         for message in self.message_buffer:
             neighboring_node.communication_terminal.receive(message)
-
-
